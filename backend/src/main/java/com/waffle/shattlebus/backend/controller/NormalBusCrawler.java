f3209fddb777a095d04e205ad9fdbdf964d93a15
GanziDaeyong <iwin1203@naver.com>
2021-02-26 01:53:12 +0900

    #2

--- backend/src/main/java/com/waffle/shattlebus/backend/controller/NormalBusCrawler.java
+++ backend/src/main/java/com/waffle/shattlebus/backend/controller/NormalBusCrawler.java
@@ -10,11 +10,16 @@ import java.net.URL;
 
 public class NormalBusCrawler {
 
-    private static String getXml(int arsId) throws Exception{
+    private static String getXml(int code, String type) throws Exception{ // 리퀘 받아오는 코드 (인코딩 문제로 jsoup과 분리.)
         String USER_AGENT = "Mozilla/5.0";
         String key = "k4UvnK2anWmh10%2BJiof8w7qWin6wmp72vRlUryHNKxrpQ5%2Fot599PY929AaGnv8KpuBh9%2FN0xe2%2F53ja9cgI6g%3D%3D";
-        String path = "http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid?ServiceKey=" + key + "&arsId=" + arsId;
-
+        String path = "";
+        if (type=="station") {
+            path = "http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid?ServiceKey=" + key + "&arsId=" + code;
+        }
+        if (type=="bus") {
+            path = ""; // 버스 노선 조회 api 링크 가져오기
+        }
         URL url = new URL(path);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
@@ -30,13 +35,20 @@ public class NormalBusCrawler {
         return response.toString();
 
     }
-    private static void findNbByStation(int arsId) throws Exception {
+    private static void findStation(int arsId) throws Exception {
 
-        String entireXml=getXml(arsId);
+        String entireXml=getXml(arsId, "station");
         Document doc = Jsoup.parse(entireXml);
         System.out.println(doc);
-
         // 여기 아래에 jsoup으로 크롤링 코드 작성 //
 
+    }
+    private static void findBusRoute(int bus_code) throws Exception {
+
+        String entireXml=getXml(bus_code, "station");
+        Document doc = Jsoup.parse(entireXml);
+        System.out.println(doc);
+        // 여기 아래에 jsoup으로 크롤링 코드 작성 //
+        
     }
 }
