40fd45c0b444709d17f2211f324f4120b58ca981
pasa3232 <pasa3232@gmail.com>
2021-03-03 21:50:03 +0900

    initial search

--- backend/src/main/java/com/waffle/shattlebus/backend/searching/SearchHangul.java
+++ backend/src/main/java/com/waffle/shattlebus/backend/searching/SearchHangul.java
@@ -1,23 +1,67 @@
 package com.waffle.shattlebus.backend.searching;
 
 public class SearchHangul {
-
+    private static final char HANGUL_BEGIN_UNICODE = 44032; // 가
+    private static final char HANGUL_LAST_UNICODE = 55203; // 힣
+    private static final char HANGUL_BASE_UNIT = 588; //각자음 마다 가지는 글자수
     static String text = "가깋낗닣딯띻맇밓빟삫싷잏짛찧칳킿팋핗힣";
-    static String jamoRef = "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅇㅈㅉㅊㅋㅌㅍㅎ";
+    static String INITIAL = "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅇㅈㅉㅊㅋㅌㅍㅎ";
 
     static char[] textArr = text.toCharArray();
-    static char[] jamoArr = jamoRef.toCharArray();
+    static char[] INITIAL_SOUND = INITIAL.toCharArray();
 
-    // 단어 검색
+    /**
+     * 해당 문자가 INITIAL_SOUND인지 검사.
+     *
+     * @param searchar
+     * @return
+     */
+    private static boolean isInitialSound(char searchar){
+        for(char c:INITIAL_SOUND){
+            if(c == searchar){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 해당 문자의 자음을 얻는다.
+     *
+     * @param c 검사할 문자
+     * @return
+     */
+    private static char getInitialSound(char c) {
+        int hanBegin = (c - HANGUL_BEGIN_UNICODE);
+        int index = hanBegin / HANGUL_BASE_UNIT;
+        return INITIAL_SOUND[index];
+    }
+
+    /**
+     * 해당 문자가 한글인지 검사
+     *
+     * @param c 문자 하나
+     * @return
+     */
+    private static boolean isHangul(char c) {
+        return HANGUL_BEGIN_UNICODE <= c && c <= HANGUL_LAST_UNICODE;
+    }
+
+    /**
+     * 단어 검색시 범위의 EndPoint를 찾음
+     *
+     * @param keyword 단어 하나
+     * @return endKeyword
+     */
     static String findEndKeyword(String keyword){
         // 마지막 글자
         char targetChar = keyword.charAt(keyword.length() - 1);
         char endChar = '0';
 
         // 입력값의 끝 문자가 ㄱ...ㅎ 인 경우
-        if(targetChar <= 12622) {
-            for(int i = 0; i < jamoArr.length; i++) {
-                if(targetChar == jamoArr[i]) {
+        if(isInitialSound(targetChar)) {
+            for(int i = 0; i < INITIAL_SOUND.length; i++) {
+                if(targetChar == INITIAL_SOUND[i]) {
                     endChar = textArr[i + 1];
                 }
             }
@@ -43,6 +87,10 @@ public class SearchHangul {
 
     // for testing
     public static void main(String[] args){
+        String a = "곰나담랑밍";
+        String b = "가나다라마";
+        String c = "갛낳닿랗맣";
+        System.out.println(a.compareTo(b)+" "+a.compareTo(c));
         System.out.println(findEndKeyword("가글"));
         System.out.println(findEndKeyword("가ㅇ"));
     }
