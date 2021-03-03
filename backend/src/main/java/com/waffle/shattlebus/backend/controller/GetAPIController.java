cc57dc4eb0d0c7026c4ef46ec7967265b0bcf3c6
CSE1JIRm9 <54717129+CSE1JIRm9@users.noreply.github.com>
2021-03-04 02:49:33 +0900

    merged all

--- backend/src/main/java/com/waffle/shattlebus/backend/controller/GetAPIController.java
+++ backend/src/main/java/com/waffle/shattlebus/backend/controller/GetAPIController.java
@@ -2,33 +2,59 @@ package com.waffle.shattlebus.backend.controller;
 import com.waffle.shattlebus.backend.model.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
-
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.json.JSONObject;
+import org.json.XML;
 
 @RestController
-@RequestMapping("/api/v1") // localhost:8080/api
+@RequestMapping("/api/v1")
 public class GetAPIController {
+
+    String USER_AGENT = "Mozilla/5.0";
+    String key = "k4UvnK2anWmh10%2BJiof8w7qWin6wmp72vRlUryHNKxrpQ5%2Fot599PY929AaGnv8KpuBh9%2FN0xe2%2F53ja9cgI6g%3D%3D";
+
+    //usage
+    String xmlString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><test>test value</test><test2>test2 value</test2>";
+    JSONObject jsonObject = XML.toJSONObject(xmlString);
 
     List<Station> stationList;
     List<Bus> busList;
 
-    // 정류장 상세
-    @GetMapping("/stations/{stationid}")  // localhost:8080/api/getParameters?id=shlee0882&email=shlee0882@gmail.com
-    public String getStations(@PathVariable("stationid") Long id){
+    // 정류장 상세 - 자체 리스트
+    @GetMapping("/stations/{stationid}")
+    public JSONObject getStations(@PathVariable("stationid") Long id) throws Exception{
 
-        // 정류장 db 사용 여부 ??
+        String path = "http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid?serviceKey="
+                + key
+                + "&arsId=" + id;
 
+        JSONObject data = getJSON(path);
+        JSONObject response = new JSONObject();
 
-        return "정류장 id 상세: " + id;
+        response.put("id", id.toString());
+        response.put("direction", data.get("nxtStn"));
+        // name은 리스트에서 받아오기
+
+        return null;
     }
 
-    // 버스 상세
+    // 버스 상세 - 자체 리스트
     @GetMapping("/buses/{busid}")
-    public String getMultiParameters(@PathVariable("busid") Long id) {
+    public String getMultiParameters(@PathVariable("busid") Long id) throws Exception {
 
         // 버스 db 사용 여부 ??
 
-        return "버스 id 상세: " + id;
+        String path = "http://ws.bus.go.kr/api/rest/busRouteInfo/getRoutePath?serviceKey="
+        + key + "&busRouteId=" + id;
+
+        return getJSON(path).toString();
     }
 
     // 정류장 검색
@@ -59,6 +85,21 @@ public class GetAPIController {
 
     }
 
+    public JSONObject getJSON(String path) throws Exception {
+
+        URL url = new URL(path);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.getResponseCode(); // 에러처리 아직 안함
+        StringBuilder response = new StringBuilder();
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine="";
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close(); // print result
+        return XML.toJSONObject(response.toString());
+    }
 }
 
 
--- backend/src/main/java/com/waffle/shattlebus/backend/controller/PostAPIController.java
+++ backend/src/main/java/com/waffle/shattlebus/backend/controller/PostAPIController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1")
 public class PostAPIController {
 
-    // http통신할때 post는 body에다 data를 집어넣어서 받아오겠다.
-    // @RequestBody에 SearchVO에 있는 값을 매칭해서 보내줘라.
+    // 여긴 그,, 할게 없나?
 
     @PostMapping(path = "/feedback/")
     public Feedback postRequest(@RequestBody Feedback feedback){
