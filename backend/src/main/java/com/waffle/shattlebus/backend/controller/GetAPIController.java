5f3e85af96bc6a23b98726bfdd20d2c10972bc5a
CSE1JIRm9 <54717129+CSE1JIRm9@users.noreply.github.com>
2021-03-17 05:10:36 +0900

    almost done

--- backend/src/main/java/com/waffle/shattlebus/backend/controller/GetAPIController.java
+++ backend/src/main/java/com/waffle/shattlebus/backend/controller/GetAPIController.java
@@ -4,14 +4,12 @@ import org.json.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Map;
+import java.util.*;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -35,7 +33,7 @@ public class GetAPIController {
     List<Station> stationList;
     List<Bus> busList;
 
-    // 정류장 상세 - 자체 리스트
+    //                                  정류장 상세
     @GetMapping("/stations/{stationid}")
     public String getStations(@PathVariable("stationid") Long id) throws Exception{
 
@@ -45,113 +43,117 @@ public class GetAPIController {
 
         //List<List<String>> Info = BusTsvInfo.getBusTsvInfo();
         Map<Long, List<String>> BTI = BusTsvInfo.getBTIMap();
-        List<String> Info = BTI.get(id);
-        JSONObject data = (JSONObject) getJSON(path).get("msgBody");
-        JSONObject response = new JSONObject();
 
-        response.put("id", id.toString());
-        response.put("name", Info.get(0));
-        response.put("direction", Info.get(1));
+        // ** 정류장이 존재하지 않는 경우
 
-        // 버스 리스트 받아오기
-        JSONArray busList = data.getJSONArray("itemList"); // 여기서 정보 추출 필요
-        JSONArray ourList = new JSONArray();
-        for(int i=0; i<busList.length(); i++){
-            JSONObject a = (JSONObject) busList.get(i);
-            JSONObject n = new JSONObject();
-            n.put( "name", a.get("rtNm").toString());
-            n.put( "id", a.get("busRouteId").toString());
-            n.put( "arrival_time", a.get("arrmsg1").toString());
-            ourList.put(n);
+        try {
+            List<String> Info = BTI.get(id);
+            JSONObject data = (JSONObject) getJSON(path).get("msgBody");
+            JSONObject response = new JSONObject();
+
+            response.put("id", id.toString());
+            response.put("name", Info.get(0));
+            response.put("direction", Info.get(1));
+
+            // ***시내*** 버스 리스트 받아오기
+            JSONArray busList = data.getJSONArray("itemList"); // 여기서 정보 추출 필요
+            JSONArray ourList = new JSONArray();
+            for (int i = 0; i < busList.length(); i++) {
+                JSONObject a = (JSONObject) busList.get(i);
+                JSONObject n = new JSONObject();
+                n.put("name", a.get("rtNm").toString());
+                n.put("id", a.get("busRouteId").toString());
+                n.put("arrival_time", a.get("arrmsg1").toString());
+                n.put("is_shuttle", false);
+                ourList.put(n);
+            }
+            response.put("buses", ourList);
+
+            // 셔틀버스 리스트 받아오기 (?)
+            // 1. 교내순환
+            // 2. 역순환
+
+            return response.toString();
         }
+        catch(Exception e){
 
-        response.put("buses", ourList);
-        // arrmsg1, rtNm, busRouteId -> 따로 파싱을 해야하나 어떻게 해야하나
-
-        return response.toString();
+            // 존재하지 않는 버스일 경우
+            throw e;
+        }
     }
 
-    // 버스 상세 - 자체 리스트
+    //                                  버스 상세
     @GetMapping("/buses/{busid}")
-    public JSONObject getBuses(@PathVariable("busid") String id) throws Exception {
+    public String getBuses(@PathVariable("busid") String id) throws Exception {
         
-        String path = "http://ws.bus.go.kr/api/rest/busRouteInfo/getRoutePath?serviceKey="
+        String path = "http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRouteAll?serviceKey="
         + key + "&busRouteId=" + id;
 
-        //List<List<String>> Info = BusTsvInfo.getBusTsvInfo();
-        Map<Long, List<String>> BTI = BusTsvInfo.getBTIMap();
-        List<String> Info = BTI.get(id);
+        // 일단 못찾는 경우는 배제하도록 하자
         JSONObject data = (JSONObject) getJSON(path).get("msgBody");
-        JSONObject response = new JSONObject();
+        JSONArray  datalist = data.getJSONArray("itemList");
 
-        response.put("id", id.toString());
-        response.put("name", Info.get(0));
-        response.put("direction", Info.get(1));
+        JSONArray  stations = new JSONArray();
+        JSONArray  operating_buses = new JSONArray();
+        JSONObject result = new JSONObject();
 
-        // 버스 리스트 받아오기
-        JSONArray busList = data.getJSONArray("itemList"); // 여기서 정보 추출 필요
-        JSONArray ourList = new JSONArray();
-        for(int i=0; i<busList.length(); i++){
-            JSONObject a = (JSONObject) busList.get(i);
-            JSONObject n = new JSONObject();
-            n.put( "name", a.get("rtNm").toString());
-            n.put( "id", a.get("busRouteId").toString());
-            n.put( "arrival_time", a.get("arrmsg1").toString());
-            ourList.put(n);
+        for (int i = 0; i < datalist.length(); i++) {
+
+            JSONObject element = (JSONObject) datalist.get(i);
+
+            JSONObject eachStop = new JSONObject();
+            eachStop.put("id", element.get("stId"));
+            eachStop.put("name", element.get("stNm"));
+            eachStop.put("idx", element.get("staOrd"));
+            stations.put(eachStop);
+
+            String arrMsg = element.get("arrmsg1").toString();
+            if(arrMsg.contains("곧 도착") || arrMsg.contains("0번째")){  // ************* note
+                JSONObject bus = new JSONObject();
+                bus.put("arriving_at", element.get("stId"));
+                bus.put("plateNo", element.get("plainNo1"));
+                operating_buses.put(bus);
+            }
+
+            if(i==1){
+                result.put("id", element.get("busRouteId"));
+                result.put("name", element.get("rtNm"));
+                result.put("is_shuttle", false);
+                result.put("allocation", element.get("term"));
+                result.put("first_bus", element.get("firstTm"));
+                result.put("last_bus", element.get("lastTm"));
+            }
         }
 
-        response.put("buses", ourList);
-        // arrmsg1, rtNm, busRouteId -> 따로 파싱을 해야하나 어떻게 해야하나
+        result.put("operating_buses", operating_buses);
+        result.put("stations", stations);
 
-        return getJSON(path);
+        return result.toString();
     }
 
-    // 정류장 검색
+    //                              정류장 검색 by 승한
     @GetMapping("/stations") 
     public String getStations(@RequestParam(value = "query", required = false) String query){
-        String[] queries = query.split(" ");
-
-        // 검색 처리
-        List<List<String>> rev = getBusTsvInfo();
-        Map<String, List<String>> result = new HashMap<>();
-        for(List<String> station_info : rev) {
-            String station_name = station_info.get(1);
-            boolean flag = true;
-            int st = 0;
-            for (int i = 0; i < queries.length; i++) {
-                st = compWord(station_name.substring(st), queries[i]);
-                if (st == -1) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) result.put(station_info.get(0), station_info.subList(1, 4));
-        }
 
         JSONObject response = new JSONObject();
-        JSONArray jArray = new JSONArray();
-        for(String key : result.keySet()){
-            JSONObject sObject = new JSONObject();
-            sObject.put("id", key);
-            sObject.put("name", result.get(key).get(0));
-            sObject.put("to", result.get(key).get(1));
-            sObject.put("dir", Integer.parseInt(result.get(key).get(2)));
-            jArray.put(sObject);
-        }
-        response.put("result", jArray);
+        response.put("result", search_SH(query, true));
+
         return response.toString();
     }
 
-    // 통합 검색
+    //                              통합 검색
     @GetMapping("/find")
     public String getData(@RequestParam(value = "query", required = false) String query){
 
-        // 검색 처리
+        JSONObject response = new JSONObject();
+        response.put("station_result", search_SH(query, true));
+        response.put("bus_result", search_SH(query, false));
 
-        return "정류장 또는 버스 이름: " + query;
+        return response.toString();
     }
 
     //길찾기
+    //********************************** v1에서는 만들지 않기로 함 **********************************************
     @GetMapping("/findpath/")
     public String getListPath(@RequestParam String from, @RequestParam String to) {
 
@@ -159,6 +161,62 @@ public class GetAPIController {
 
         return "길찾기 결과: " + from + " & " + to;
 
+    }
+
+    public JSONArray search_SH(String query, boolean isStation){
+
+        String[] queries = query.split(" ");
+
+        // 검색 처리
+
+        List<List<String>> rev;
+        Map<String, List<String>> result_st = new HashMap<>();
+        Map<String, String> result_bus = new HashMap<>();
+
+        if(isStation)
+            rev = getBusTsvInfo();
+        else
+            rev = getBuses();
+
+
+        for (List<String> info : rev) {
+            String name = info.get(1);
+            boolean flag = true;
+            int st = 0;
+            for (String s : queries) {
+                st = compWord(name.substring(st), s);
+                if (st == -1) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                if(isStation) result_st.put(info.get(0), info.subList(1, 4));
+                else result_bus.put(info.get(1), info.get(0));
+            }
+        }
+
+
+        JSONArray jArray = new JSONArray();
+        if(isStation) {
+            for (String key : result_st.keySet()) {
+                JSONObject sObject = new JSONObject();
+                sObject.put("id", key);
+                sObject.put("name", result_st.get(key).get(0));
+                sObject.put("to", result_st.get(key).get(1));
+                sObject.put("dir", Integer.parseInt(result_st.get(key).get(2)));
+                jArray.put(sObject);
+            }
+        }
+        else{
+            for (String key : result_bus.keySet()) {
+                JSONObject sObject = new JSONObject();
+                sObject.put("id", result_bus.get(key));
+                sObject.put("name", key);
+                jArray.put(sObject);
+            }
+        }
+        return jArray;
     }
 
     public JSONObject getJSON(String path) throws Exception {
@@ -175,6 +233,26 @@ public class GetAPIController {
         }
         in.close(); // print result
         return (JSONObject) (XML.toJSONObject(response.toString())).get("ServiceResult");
+    }
+
+    static List<List<String>> getBuses() {
+
+        List<List<String>> buses = new ArrayList<>();
+
+        try {
+            String path = "src/main/java/com/waffle/shattlebus/backend/controller/Buses.tsv";
+
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String line = "";
+            while((line = br.readLine())!=null) {
+                String[] eachLineSplit = line.split("\t");
+                buses.add(Arrays.asList(eachLineSplit));
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return buses;
     }
 }
 
