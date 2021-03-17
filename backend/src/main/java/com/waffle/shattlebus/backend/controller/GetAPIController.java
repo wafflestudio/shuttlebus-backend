b9b581fcc4e649588b00e8c4b143a94e42657a14
CSE1JIRm9 <54717129+CSE1JIRm9@users.noreply.github.com>
2021-03-17 12:13:30 +0900

    trivial change

--- backend/src/main/java/com/waffle/shattlebus/backend/controller/GetAPIController.java
+++ backend/src/main/java/com/waffle/shattlebus/backend/controller/GetAPIController.java
@@ -108,9 +108,9 @@ public class GetAPIController {
             stations.put(eachStop);
 
             String arrMsg = element.get("arrmsg1").toString();
-            if(arrMsg.contains("곧 도착") || arrMsg.contains("0번째")){  // ************* note
+            if(arrMsg.contains("0번째") || arrMsg.contains("곧 도착")){  // ************* note
                 JSONObject bus = new JSONObject();
-                bus.put("arriving_at", element.get("stId"));
+                bus.put("arriving_at", element.get("stNm"));
                 bus.put("plateNo", element.get("plainNo1"));
                 operating_buses.put(bus);
             }
