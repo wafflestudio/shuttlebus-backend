5e75796b7e22ca66a250f448088a0736cdfa40ce
Jeong Daeyong <ganzidaeyong@wafflestudio.com>
2021-11-12 15:56:42 +0900

    pw changed

--- backend/src/main/java/com/waffle/shattlebus/backend/Controller/PostAPIController.java
+++ backend/src/main/java/com/waffle/shattlebus/backend/Controller/PostAPIController.java
@@ -23,8 +23,8 @@ public class PostAPIController {
     private void gmailSend(String content) {
 
         String user = "waffleshattlebus@gmail.com"; // 네이버일 경우 네이버 계정, gmail경우 gmail 계정
-        String password = "shattlebus";   // 패스워드
-
+      //  String password = "shattlebus";   // 패스워드
+	String password = "xivygdorbsvzxdje";
         // SMTP 서버 정보를 설정한다.
         Properties prop = new Properties();
 	prop.put("mail.smtp.host", "smtp.gmail.com");
