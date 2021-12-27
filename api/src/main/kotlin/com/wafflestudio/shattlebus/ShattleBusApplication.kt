bf2de75edee73d93aab2b85ba72d3c2c75e2786b
강지혁 <wlgur7238@snu.ac.kr>
2021-12-28 02:40:55 +0900

    add Application Class

---
+++ api/src/main/kotlin/com/wafflestudio/shattlebus/ShattleBusApplication.kt
@@ -0,0 +1,11 @@
+package com.wafflestudio.shattlebus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShattleBusApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShattleBusApplication>()
+}
