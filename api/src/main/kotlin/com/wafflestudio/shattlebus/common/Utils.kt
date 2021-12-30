efb4e4092298fb2bfafd7b8ea4309e18e914585b
강지혁 <wlgur7238@snu.ac.kr>
2021-12-31 02:45:25 +0900

    공공 API XML 파싱을 위한 클래스 세팅

--- api/src/main/kotlin/com/wafflestudio/shattlebus/common/Utils.kt
+++ api/src/main/kotlin/com/wafflestudio/shattlebus/common/Utils.kt
@@ -1,6 +1,18 @@
 package com.wafflestudio.shattlebus.common
 
-object Utils {
-    fun <T> Map<T, Any>.safeGet(idx: T) =
-        this[idx]
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.json.JSONArray
+import org.json.JSONObject
+import kotlin.reflect.KClass
+
+inline fun <reified T : Any> JSONArray.convertTo(type: KClass<T>): List<T> {
+    val objectMapper = ObjectMapper()
+    return this.map { json ->
+        objectMapper.readValue(json.toString(), type.java)
+    }
+}
+
+inline fun <reified T : Any> JSONObject.convertTo(type: KClass<T>): T {
+    val objectMapper = ObjectMapper()
+    return objectMapper.readValue(toString(), type.java)
 }
