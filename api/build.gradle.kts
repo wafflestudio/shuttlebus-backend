288643fb081f23e06d35e63e50dfa3ea3121d2a6
강지혁 <wlgur7238@snu.ac.kr>
2021-12-31 02:44:43 +0900

    base 세팅

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,3 +1,7 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    // jsoup to parse XML
+    api(group = "org.jsoup", name = "jsoup", version = "1.10.2")
+    api(group = "org.json", name = "json", version = "20180813")
 }
